47cbbe5b0ce758670793e267b2716a9bb2d92b89
Mohamed <mnogot@orsys.co.ma>
2023-11-20 12:04:07 +0000

    Initial commit

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -1,19 +1,19 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { startFetchMealByCategory } from "../../actions/mealsActions";
+import { useMealContext } from "../../context/mealContext";
 import "./CategoryPage.scss";
-import { useMealContext } from '../../context/mealContext';
-import MealList from '../../components/Meal/MealList';
-import { useParams } from 'react-router-dom';
-import { startFetchMealByCategory } from '../../actions/mealsActions';
 
 const CategoryPage = () => {
-  const {name} = useParams();
+  const { name } = useParams();
   const { categoryMeals, dispatch, categories } = useMealContext();
   let catDescription = "";
 
-  if(categories){
-    categories.forEach(category => {
-      if(category?.strCategory === name) catDescription = category?.strCategoryDescription;
-    })
+  if (categories) {
+    categories.forEach((category) => {
+      if (category?.strCategory === name)
+        catDescription = category?.strCategoryDescription;
+    });
   }
 
   useEffect(() => {
@@ -21,18 +21,15 @@ const CategoryPage = () => {
   }, [name, dispatch]);
 
   return (
-    <main className='main-content py-5'>
-      <div className='container'>
-        <div className='cat-description px-4 py-4'>
-          <h2 className='text-orange fw-8'>{name}</h2>
-          <p className='fs-18 op-07'>{catDescription}</p>
+    <main className="main-content py-5">
+      <div className="container">
+        <div className="cat-description px-4 py-4">
+          <h2 className="text-orange fw-8">{name}</h2>
+          <p className="fs-18 op-07">{catDescription}</p>
         </div>
       </div>
-      {
-        (categoryMeals?.length) ? <MealList meals = { categoryMeals } /> : null
-      }
     </main>
-  )
-}
+  );
+};
 
-export default CategoryPage
+export default CategoryPage;
